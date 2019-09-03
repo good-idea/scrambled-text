46d7b799675b51321c5409e6b1413eedf6f55fba
Joseph Thomas <joseph@good-idea.studio>
2019-09-02 22:01:38 -0700

    feat: add useStopwatch to use in useScrambledText

---
+++ src/__tests__/useStopwatch.test.ts
@@ -0,0 +1,59 @@
+import lolex from 'lolex'
+import { act, renderHook } from '@testing-library/react-hooks'
+import { wait } from '@testing-library/react'
+import { useStopwatch } from '../react/useStopwatch'
+
+let clock
+beforeEach(() => {
+  clock = lolex.install()
+})
+
+afterEach(() => {
+  clock.uninstall()
+})
+
+const tick = async (amt: number = 100) => {
+  act(() => {
+    clock.tick(amt)
+  })
+  await wait()
+}
+
+describe('useStopwatch', () => {
+  it('should return an "elapsed" number', async () => {
+    const { result } = renderHook(() => useStopwatch(true))
+    expect(result.current.elapsed).toBeGreaterThanOrEqual(0)
+  })
+
+  it('should return the correct elapsed time', async () => {
+    const { result } = renderHook(() => useStopwatch(true))
+    expect(result.current.elapsed).toBe(0)
+    await tick(100)
+    expect(result.current.elapsed).toBe(100)
+    await tick(100)
+    expect(result.current.elapsed).toBe(200)
+    await tick(300)
+    expect(result.current.elapsed).toBe(500)
+  })
+
+  it('should start and stop the counter according to "running"', async () => {
+    const { result, rerender } = renderHook((running: boolean = true) =>
+      useStopwatch(running),
+    )
+    expect(result.current.elapsed).toBe(0)
+    await tick(100)
+    expect(result.current.elapsed).toBe(100)
+
+    /* Stop the timer */
+    rerender(false)
+    await tick(100)
+    expect(result.current.elapsed).toBe(100)
+    await tick(100)
+    expect(result.current.elapsed).toBe(100)
+
+    /* Restart the timer */
+    rerender(true)
+    await tick(100)
+    expect(result.current.elapsed).toBe(200)
+  })
+})
